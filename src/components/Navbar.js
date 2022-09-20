ed3ca3c650a60f881154a571888bef435f728b3c
DewanshiYadav <yadav7@uwindsor.ca>
2022-09-20 16:35:58 -0400

    Front-end registration form

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">Navbar</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
